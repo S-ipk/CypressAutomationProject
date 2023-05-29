b8c95d66881b70bc24253967ce7704069e9ba6ad
sipek <sipek@appannie.com>
2023-05-29 16:02:26 +0100

    api testing

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
   e2e: {
 
     baseUrl: 'https://practice.cydeo.com',
+    env: {
+      login:"/login",
+      apiUrl:"https://demoqa.com",
+      apiBooks:"/BookStore/v1/Books",
+      generateUser:"/Account/v1/User",
+      generateToken:"/Account/v1/GenerateToken",
+      loginAPI:"/Account/v1/Login"
+    },
     video: false, 
     retires: 2,
     defaultCommandTimeout: 5000, // normally defaul is 4 seconds
